be6bf68f4f10cc09cf62e05a7033715ca0e80acc
Valdeir S <valdeirpsr@hotmail.com.br>
2026-01-11 14:20:07 -0300

    feat: cria a página loa.html
    Ela exibirá para o usuário as informações da Lei Orçamentária Anual de
    forma organizada e resumida.

---
+++ src/pages/loa.ts
@@ -0,0 +1,20 @@
+import { LoaRepository } from "../domain/repositories/loa.repository";
+import { TablePresenter } from "../presentation/presenters/loa/table.presenter";
+import loaData from '../assets/json/loa-2026.json';
+import { createIcons, icons } from "lucide";
+import { TreemapPresenter } from "../presentation/presenters/loa/treemap.presenter";
+import { LoaService } from "../domain/services/loa.service";
+import { GraphLinePresenter } from "../presentation/presenters/loa/graph-line.presenter";
+
+const loaRepository = new LoaRepository(loaData);
+const loaService = new LoaService(loaRepository);
+
+const table = new TablePresenter(loaService)
+const treemap = new TreemapPresenter(loaService);
+const graphLine = new GraphLinePresenter(loaService);
+
+table.initialize();
+treemap.render()
+graphLine.initialize();
+
+createIcons({ icons })
