a9e2d02a7c8610432829737f7ad2fa7335a8683c
Valdeir S <valdeirpsr@hotmail.com.br>
2026-01-11 14:06:30 -0300

    feat: cria service para trabalhar com dados do LOA

---
+++ src/domain/services/loa.service.interface.ts
@@ -0,0 +1,48 @@
+import type { Agency, Expenses, GeneralBudget, GovernmentalSector, ItemHistory } from "../../types/loa";
+
+export interface ILoaService {
+  /**
+   * Captura total de despesas
+   */
+  getTotalRevenues(): number;
+
+  /**
+   * Captura total de despesas do Orçamento Fiscal
+   */
+  totalFiscalBudgetExpenses(): number;
+
+  /**
+   * Captura total de despesas com Seguridade Social
+   */
+  totalSocialSecurityExpenses(): number;
+
+  /**
+   * Captura as receitas gerais
+   */
+  getRevenues(): GeneralBudget;
+
+  /**
+   * Captura as despesas
+   */
+  getExpenses(): Expenses;
+
+  /**
+   * Captura o histórico de receitas
+   */
+  getRevenueHistory(): Array<ItemHistory>;
+
+  /**
+   * Captura o histórico de despesas
+   */
+  getExpenseHistory(): Array<ItemHistory>;
+
+  /**
+   * Captura as despesas por órgãos/secretarias
+   */
+  getExpensesByAgency(): Array<Agency>;
+
+  /**
+   * Captura as despesas por funções do governo
+   */
+  getExpensesGroupedFunction(): Array<GovernmentalSector>;
+}
---
+++ src/domain/services/loa.service.ts
@@ -0,0 +1,47 @@
+import type { Agency, Expenses, GeneralBudget, GovernmentalSector, ItemHistory } from "../../types/loa";
+import type { LoaRepository } from "../repositories/loa.repository";
+import type { ILoaService } from "./loa.service.interface";
+
+export class LoaService implements ILoaService {
+  private repository: LoaRepository;
+
+  constructor(loaRepository: LoaRepository) {
+    this.repository = loaRepository;
+  }
+
+  public getTotalRevenues(): number {
+    return 0;
+  }
+
+  public totalFiscalBudgetExpenses(): number {
+    return 0;
+  }
+
+  public totalSocialSecurityExpenses(): number {
+    return 0;
+  }
+
+  public getRevenues(): GeneralBudget {
+    return this.repository.getRevenues();
+  }
+
+  public getExpenses(): Expenses {
+    return this.repository.getExpenses();
+  }
+
+  public getRevenueHistory(): Array<ItemHistory> {
+    return this.repository.getHistory().filter((value) => value.tipo.startsWith('receita'));
+  }
+
+  public getExpenseHistory(): Array<ItemHistory> {
+    return this.repository.getHistory().filter((value) => value.tipo.startsWith('despesa'));
+  }
+
+  public getExpensesByAgency(): Array<Agency> {
+    return this.repository.getExpensesByAgency();
+  }
+
+  public getExpensesGroupedFunction(): Array<GovernmentalSector> {
+    return this.repository.getExpensesByFunction();
+  }
+}
